cad21573f8eb6605d66cd68c6d12b1e134277566
reacthemesjabirs <reacthemesjabirs@gmail.com>
2026-04-16 12:50:19 +0600

    clean

--- assets/js/plugins/contact-form.js
+++ assets/js/plugins/contact-form.js
@@ -1,48 +1,68 @@
 (function ($) {
     'use strict';
-    // Get the form.
-    var form = $('#contact-form');
 
-    // Get the messages div.
+    var form = $('.contact__form-wrapper');
+
     var formMessages = $('#form-messages');
 
-    // Set up an event listener for the contact form.
     $(form).submit(function (e) {
-        // Stop the browser from submitting the form.
         e.preventDefault();
 
-        // Serialize the form data.
+        // Basic client-side validation
+        var fname = $('#fname').val().trim();
+        var lname = $('#lname').val().trim();
+        var email = $('#email').val().trim();
+        var message = $('#message').val().trim();
+        var terms = $('#terms').is(':checked');
+
+        if (!fname || !lname || !email || !message) {
+            formMessages.removeClass('success').addClass('error');
+            formMessages.text('Please fill in all required fields.');
+            return false;
+        }
+
+        if (!terms) {
+            formMessages.removeClass('success').addClass('error');
+            formMessages.text('Please accept the Terms and Conditions.');
+            return false;
+        }
+
+        // Basic email validation
+        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            formMessages.removeClass('success').addClass('error');
+            formMessages.text('Please enter a valid email address.');
+            return false;
+        }
+
+        // Clear any previous messages
+        formMessages.removeClass('success error').text('');
+
         var formData = $(form).serialize();
 
-        // Submit the form using AJAX.
         $.ajax({
             type: 'POST',
             url: $(form).attr('action'),
             data: formData
         })
-            .done(function (response) {
-                // Make sure that the formMessages div has the 'success' class.
-                $(formMessages).removeClass('error');
-                $(formMessages).addClass('success');
+        .done(function (response) {
 
-                // Set the message text.
-                $(formMessages).text(response);
+            formMessages.removeClass('error').addClass('success');
+            formMessages.text(response);
 
-                // Clear the form.
-                $('#name, #email, #message').val('');
-            })
-            .fail(function (data) {
-                // Make sure that the formMessages div has the 'error' class.
-                $(formMessages).removeClass('success');
-                $(formMessages).addClass('error');
+            $('#fname, #lname, #email, #message').val('');
+            $('#terms').prop('checked', false);
+        })
+        .fail(function (data) {
 
-                // Set the message text.
-                if (data.responseText !== '') {
-                    $(formMessages).text(data.responseText);
-                } else {
-                    $(formMessages).text('Oops! An error occured and your message could not be sent.');
-                }
-            });
+            formMessages.removeClass('success').addClass('error');
+
+            if (data.responseText !== '') {
+                formMessages.text(data.responseText);
+            } else {
+                formMessages.text('Oops! An error occured and your message could not be sent.');
+            }
+        });
     });
 
 })(jQuery);
