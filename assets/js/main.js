5f1405411a224a5cc2b1bcdf86585d4bbc169a86
reacthemesjabirs <reacthemesjabirs@gmail.com>
2026-04-19 12:31:04 +0600

    clean

--- assets/js/main.js
+++ assets/js/main.js
@@ -310,10 +310,6 @@
           spaceBetween: 30,
           loop: true,
           speed: 1000,
-          // autoplay: {
-          //   delay: 3000,
-          //   disableOnInteraction: false,
-          // },
           pagination: {
             el: '.shop__pagination',
             clickable: true,
